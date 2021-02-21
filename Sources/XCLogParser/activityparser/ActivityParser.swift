a99a25055d3008146f463622a798ba77dcf5ac71
Erick Camacho <ecamacho.mx@gmail.com>
2021-02-21 16:19:52 +0100

    Use autoreleasepool to reduce memory used

--- Sources/XCLogParser/activityparser/ActivityParser.swift
+++ Sources/XCLogParser/activityparser/ActivityParser.swift
@@ -46,12 +46,8 @@ public class ActivityParser {
     public func parseActivityLogInURL(_ logURL: URL,
                                       redacted: Bool,
                                       withoutBuildSpecificInformation: Bool) throws -> IDEActivityLog {
-        let logLoader = LogLoader()
-        let content = try logLoader.loadFromURL(logURL)
-        let lexer = Lexer(filePath: logURL.path)
-        let tokens = try lexer.tokenize(contents: content,
-                                        redacted: redacted,
-                                        withoutBuildSpecificInformation: withoutBuildSpecificInformation)
+        let tokens = try getTokens(logURL, redacted: redacted,
+                                   withoutBuildSpecificInformation: withoutBuildSpecificInformation)
         return try parseIDEActiviyLogFromTokens(tokens)
     }
 
@@ -236,6 +232,21 @@ public class ActivityParser {
         return IDEActivityLogAnalyzerControlFlowStepEdge(
                                      startLocation: try parseDocumentLocation(iterator: &iterator),
                                      endLocation: try parseDocumentLocation(iterator: &iterator))
+    }
+
+    private func getTokens(_ logURL: URL,
+                           redacted: Bool,
+                           withoutBuildSpecificInformation: Bool) throws -> [Token] {
+        let logLoader = LogLoader()
+        var tokens: [Token] = []
+        try autoreleasepool {
+            let content = try logLoader.loadFromURL(logURL)
+            let lexer = Lexer(filePath: logURL.path)
+            tokens = try lexer.tokenize(contents: content,
+                                            redacted: redacted,
+                                            withoutBuildSpecificInformation: withoutBuildSpecificInformation)
+        }
+        return tokens
     }
 
     private func parseMessages(iterator: inout IndexingIterator<[Token]>) throws -> [IDEActivityLogMessage] {
