5e36c7399744991809ea412562f592e1cb8b2ec6
Danny Gilbert <spydercapriani@gmail.com>
2022-02-02 09:42:26 -0600

    Updates Package Dependencies
    CryptoSwift: 1.3.3 -> 1.4.2
    ArgumentParser: 0.3.1 -> 1.0.3
    Signed-off-by: Danny Gilbert <spydercapriani@gmail.com>

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/1024jp/GzipSwift", from: "5.1.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.3.3")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.3"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         .target(
